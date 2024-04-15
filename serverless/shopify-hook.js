ddf59352a40f284612ce5f10bf9841a1cb32da9b
rkeerthient <rkeerthi@yext.com>
2024-04-16 02:02:31 +0530

    trst upfatf

--- serverless/shopify-hook.js
+++ serverless/shopify-hook.js
@@ -1,8 +1,8 @@
-const handler = async (event, context) => {
-  console.log(JSON.stringify(event));
+const handler = async (req, context) => {
+  console.log(JSON.stringify(req));
   console.log(JSON.stringify(context));
   try {
-    const { city, country } = context.params;
+    const { city, country } = JSON.parse(req.body);
     return { statusCode: 500, body: `You're visiting ${city} in ${country}!` };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
