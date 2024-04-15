61d77e2ff33ab18c0eb6440de00bb6f257cf6247
rkeerthient <rkeerthi@yext.com>
2024-04-16 01:36:26 +0530

    added serverless

---
+++ serverless/shopify-hook.js
@@ -0,0 +1,12 @@
+const handler = async (req, context) => {
+  console.log(JSON.stringify(req));
+  console.log(JSON.stringify(context));
+  try {
+    const { city, country } = context.params;
+    return { statusCode: 500, body: `You're visiting ${city} in ${country}!` };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
