fac4e7e9f082be80c8fc08a29441c02a14b55e2c
rkeerthient <rkeerthi@yext.com>
2024-04-29 23:58:32 +0530

    upfated

--- serverless/shopify-hook.js
+++ serverless/shopify-hook.js
@@ -1,5 +1,6 @@
 const handler = async (req, context) => {
   console.log(JSON.stringify(req));
+  console.log(JSON.stringify(context));
   try {
     const { city, country } = JSON.parse(req.body);
     return { statusCode: 500, body: `You're visiting ${city} in ${country}!` };
