fd78623640d0a2c799be37b34998528c61d9b18c
rkeerthient <rkeerthi@yext.com>
2024-05-01 22:02:21 +0530

    fixed update query

--- serverless/shopify-hook.js
+++ serverless/shopify-hook.js
@@ -16,33 +16,33 @@ const handler = async (req, context) => {
   for (const variant of variants) {
     variant.option1 === latestUpdated.option1 &&
       productVariants.push({
-        entityId: variant.id,
+        entityId: variant.id.toString(),
         sku: variant.sku,
         size: variant.option2,
-        inventory: variant.inventory_quantity,
+        inventory: variant.inventory_quantity.toString(),
       });
   }
   const variantToUpdate = productVariants[0].entityId;
   console.log(variantToUpdate);
-  const c_greysonSizeDetails = {
+  const sizeDetailsToUpdate = {
     c_greysonSizeDetails: productVariants,
   };
 
   console.log(JSON.stringify(c_greysonSizeDetails));
 
-  // await fetch(
-  //   `https://${
-  //     process.env.ENV_TYPE === "PRODUCTION" ? "api" : "sbx-api"
-  //   }.us.yextapis.com/v2/accounts/me/entities/${variantToUpdate}?api_key=${
-  //     process.env.API_KEY
-  //   }&v=20220101`,
-  //   {
-  //     method: "PUT",
-  //     body: JSON.stringify(c_greysonSizeDetails),
-  //   }
-  // )
-  //   .then(() => console.log("Success"))
-  //   .catch((e) => console.log(e.message));
+  await fetch(
+    `https://${
+      process.env.ENV_TYPE === "PRODUCTION" ? "api" : "sbx-api"
+    }.us.yextapis.com/v2/accounts/me/entities/${variantToUpdate}?api_key=${
+      process.env.API_KEY
+    }&v=20220101`,
+    {
+      method: "PUT",
+      body: JSON.stringify(sizeDetailsToUpdate),
+    }
+  )
+    .then(() => console.log("Success"))
+    .catch((e) => console.log(e.message));
 };
 
 module.exports = { handler };
